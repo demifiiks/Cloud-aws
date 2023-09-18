bc817f2172c41767bd87490d51d516a86ec8ea40
demifiiks <shodemodupe@gmail.com>
2023-09-18 10:57:10 +0100

    to remove my sensitive data on helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -6,9 +6,7 @@ from sqlalchemy import create_engine
 region = 'eu-west-1'
 bucket_name = 'payminute'
 
-access_key = 'AKIA2A3WC5YS3DXRXJWI'
-secret_key = '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'
- 
+
 
 
 def create_bucket():
